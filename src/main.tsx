915f8a70ff33cf1e9b7a6f6e48ab5ca3fff9cff5
Lucas Kennedy <lukakfp@gmail.com>
2024-02-08 21:48:08 -0300

    feat: Config mantine provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
+import "@mantine/core/styles.css";
+
 import { App } from "./App.tsx";
+import { MantineProvider } from "@mantine/core";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider defaultColorScheme="auto">
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
