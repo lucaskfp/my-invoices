28e8d49f3025f34c8add8dc5ac5061cf3656a49a
Lucas Kennedy <lukakfp@gmail.com>
2024-02-10 15:25:42 -0300

    chore: Add matine dates

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,27 @@
 import "@mantine/core/styles.css";
-import './index.css'
+import '@mantine/dates/styles.css';
+import "./index.css";
+
+import { Button, MantineProvider, createTheme } from "@mantine/core";
 
 import { App } from "./App.tsx";
-import { MantineProvider } from "@mantine/core";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const theme = createTheme({
+  components: {
+    Button: Button.extend({
+      defaultProps: {
+        size: "md",
+        radius: "xl",
+      },
+    }),
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MantineProvider defaultColorScheme="auto">
+    <MantineProvider defaultColorScheme="auto" theme={theme}>
       <App />
     </MantineProvider>
   </React.StrictMode>
